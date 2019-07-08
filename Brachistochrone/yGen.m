05891e5d69fecd7aaf1446cb56cbf0101826f285
Xinyou Han <b4baker@gmail.com>
2019-07-08 10:05:36 +0900

    rewrite curve generation

--- Brachistochrone/yGen.m
+++ Brachistochrone/yGen.m
@@ -1,7 +1,10 @@
 function [output] = yGen(funcType, input)
-    if funcType == 1
-        output = -input;
-    else
-        output = -input;
+    switch funcType
+        case 1
+            output = -input;
+        case 2
+            output = -sqrt(100-(input-10).^2);
+        otherwise
+            output = -input;
     end
 end
