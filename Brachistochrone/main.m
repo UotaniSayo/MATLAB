f12878e21a4a615954648071ece7775015726304
Xinyou Han <b4baker@gmail.com>
2019-07-10 12:48:19 +0900

    add animation block
    pause is not good though

--- Brachistochrone/main.m
+++ Brachistochrone/main.m
@@ -1,8 +1,9 @@
 % start: (0,0)
+clear;
 
 %params
 g = 0.98;
-step = 0.01;
+step = 0.02;
 %goal: (10,-10)
 xGoal = 10;
 yGoal = 10;
@@ -30,16 +31,32 @@ for i=1:len-1
     temp = vArray(i)^2+2*acc*arcLen(i);
     if temp < 0
         stopFlag = true;
-        break
+        break;
     end
     tArray(i) = (sqrt(temp)-vArray(i))/acc;
     vArray(i+1) = vArray(i)+tArray(i)*acc;
 end
 
-tAll = sum(tArray);
 if not(stopFlag)
+    tAll = sum(tArray);
     disp('Time:');
     disp(tAll);
 else
     disp('Goal cannot be arrived');
+    return;
 end
+
+%animation
+mainFig = figure();
+mainFig.Position = [600 400 500 500];
+hold on;
+plot(xArray,yArray);
+obj = plot(xArray(1),yArray(1),'o');
+
+tic
+for i=1:len-1
+    pause(tArray(i));
+    obj.XData = xArray(i+1);
+    obj.YData = yArray(i+1);
+end
+toc
